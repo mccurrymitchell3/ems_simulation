c9c289e67b56eeff4b097a830dceed7158b40b7e
mccurrymitchell3 <mccurrymitchell@gmail.com>
2020-02-01 18:45:22 -0500

    changed scheduling technique

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import queue
 import time
+from enum import Enum
 
 class Station:
 
@@ -11,23 +12,30 @@ class Station:
 
     self.stationQueue = queue.PriorityQueue() # Queue of ambulances to depart
 
-    def insertRequest(self, request):
-        self.stationQueue.insert(request)
+    # Insert a new call request to the priority queue
+    def insertEvent(self, event):
+        self.stationQueue.insert(event)
 
     def dispatch(self):
         # Keep track of time. All requests take the same amount of time.
-        request = self.stationQueue.get() # Take the next EMS request from the queue
         self.ambulancesAtStation -= 1 # Update number of ambulances at the station and those already dispatched
         self.ambulancesEnRoute += 1
-        # After avgWaitingTime call ambulance returned
-        time.sleep(self.avgWaitTime)
-        self.returned()
+
+        # After avgWaitTimeFromData call ambulance returned
+        self.insertEvent((arrive, time.clock + self.avgWaitTimeFromData))
 
     # After ambulance returns
     def returned(self):
         self.ambulancesAtStation += 1
         self.ambulancesEnRoute -= 1
 
+    request = self.stationQueue.get() # Take the next EMS request from the queue
+
     # If there are ambulances available at the station, dispatch them
     if self.ambulancesAtStation > 0:
         self.dispatch()
+
+class Event(Enum):
+    depart = 1 # Ambulance departs the station
+    arrive = 2 # Ambulance completes its route and arrives back at the station
+    newCall = 3 # New EMS Call

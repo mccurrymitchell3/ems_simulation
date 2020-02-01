083fbb981065c0b8796eb6b6d5adc9d74e41e5cd
mccurrymitchell3 <mccurrymitchell@gmail.com>
2020-02-01 17:03:07 -0500

    Added some details to Station

--- main.py
+++ main.py
@@ -4,19 +4,26 @@ class Station:
 
     self.ambulancesAtStation = 0
     self.ambulancesEnRoute = 0
-    self.avgWaitTime = 0
+    self.avgWaitTime = 20.0 # minutes
+    self.totalWaitingTime = 0.0
 
-    self.stationQueue = queue.PriorityQueue()
+    self.stationQueue = queue.PriorityQueue() # Queue of ambulances to depart
 
     def insertRequest(self, request):
         self.stationQueue.insert(request)
 
-    def dispatch(self, request):
+    def dispatch(self):
         # Keep track of time. All requests take the same amount of time.
-        request = self.stationQueue.get()
-        self.ambulancesAtStation -= 1
+        request = self.stationQueue.get() # Take the next EMS request from the queue
+        self.ambulancesAtStation -= 1 # Update number of ambulances at the station and those already dispatched
         self.ambulancesEnRoute += 1
+        # After avgWaitingTime call ambulance returned
 
-        # After ambulance returns
+    # After ambulance returns
+    def returned(self):
         self.ambulancesAtStation += 1
         self.ambulancesEnRoute -= 1
+
+    # If there are ambulances available at the station, dispatch them
+    if self.ambulancesAtStation > 0:
+        self.dispatch()

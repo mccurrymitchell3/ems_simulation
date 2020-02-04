1bde647b2215fe671ac9cd97f24dfae0f6dea702
samanthadaugherty <sedaugherty6@gmail.com>
2020-02-03 20:16:21 -0500

    added more comments to simulation

--- simulation.py
+++ simulation.py
@@ -6,6 +6,8 @@ import generator
 globals.init()
 output = []
 
+# we run the simulation for a varying number of ambulances per station, documenting
+# the average wait time depending on number of ambulances available
 for numAmbulances in range(1, 10):
 
     s1 = station.Station(numAmbulances, 15) # (Number of ambulances available at the station,
@@ -16,7 +18,7 @@ for numAmbulances in range(1, 10):
 
     cc = callcenter.CallCenter()
     gen = generator.Generator()
-    duration = 300 # minutes to run
+    duration = 300 # minutes to run simulation
 
     # Run for duration minutes
     while globals.now < duration:
@@ -33,8 +35,9 @@ for numAmbulances in range(1, 10):
         # While there are elements in the priority queue, process the next element
         while (not s1.stationQueue.empty() or not s1.waitingQueue.empty()) and globals.now < startTime + 15:
             s1.process_next_elem(startTime)
-        
-        # Reset start time for station two
+
+        # Reset start time for station two, so that it technically is running
+        # at the same time as station one but without multithreading
         globals.now = startTime
         print("STATION 2")
         while (not s2.stationQueue.empty() or not s2.waitingQueue.empty()) and globals.now < startTime + 15:
@@ -51,7 +54,7 @@ for numAmbulances in range(1, 10):
     print("Station One Average Waiting Time: ", s1.totalWaitingTime / s1.callEventsProcessed)
     print("Station Two Average Waiting Time: ", s2.totalWaitingTime / s2.callEventsProcessed)
 
-    # Add average waiting times for each call received to output list. 
+    # Add average waiting times for each call received to output list.
     # These stats will be written to the 'output.txt'.
     output.append("Number of Ambulances: {}\n".format(numAmbulances))
     output.append("Station One Average Waiting Time: {}\n".format(s1.totalWaitingTime / s1.callEventsProcessed))
